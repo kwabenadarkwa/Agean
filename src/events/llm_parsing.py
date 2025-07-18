45f3bc382ff4f05b78f315ac76879627494c5a26
kwabenadarkwa <darkwak@live.com>
2025-07-18 11:11:23 +0100

    adds communication with llm server

--- src/events/llm_parsing.py
+++ src/events/llm_parsing.py
@@ -1,10 +1,23 @@
-from event_pipeline.base import EventBase
+import json
 import os
+
+import requests
 from dotenv import load_dotenv
+from event_pipeline.base import EventBase
 
 load_dotenv()
-#use the ip from the .env file to access the api
 
 
-class LLMParse(EventBase):
-    pass
+# TODO: think about giving the AI some examples that it could use to give me a good response
+# class LLMParse(EventBase):
+#     pass
+
+
+if __name__ == "__main__":
+    address = f"http://{os.getenv("SERVER_IP")}/api/generate"
+    response = requests.post(
+        url=address,
+        headers={"Content-Type": "application/json"},
+        data=json.dumps({"model": "deepseek-r1:7b", "prompt": "Hello, world!"}),
+    )
+    print(response.text)
